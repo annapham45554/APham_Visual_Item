dc9b2a71da8c0d5f09e9325f94cebc8f8dbe11c5
annapham45554 <phamnguyenlinhtruc@gmail.com>
2024-10-07 00:10:09 -0700

    adding gif

--- js/p5.js
+++ js/p5.js
@@ -1,7 +1,35 @@
-function setup() {
-    createCanvas(400, 400);
-  }
+let cats = [];
+let currentCat;
+let birthdayCard;
+let catDisplayed = false;
+
+function preload() {
+  cats.push(loadImage('cat1.gif'));
+  cats.push(loadImage('cat2.gif'));
+  cats.push(loadImage('cat3.gif'));
+  cats.push(loadImage('cat4.gif'));
   
-  function draw() {
-    background(220);
+  birthdayCard = loadImage('birthday_card.png'); 
+}
+
+function setup() {
+  createCanvas(600, 600);
+    background(44,43,65);
+    currentCat = random(cats);
+
+}
+
+function draw() {
+  background(44, 43, 65); 
+
+  if (catDisplayed === true) {
+    image(currentCat, 10, 120, 700, 380);
+  } else {
+    image(birthdayCard, 105, 110, 350, 400);
   }
+}
+
+function mousePressed() {
+  currentCat = random(cats);
+  catDisplayed = true; 
+}
